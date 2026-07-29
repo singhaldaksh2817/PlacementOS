6b73cae775a9c3d82dfca643ec731b1bf3fbb279
singhaldaksh2817 <singhaldaksh2817@gmail.com>
2026-07-29 19:05:02 +0530

    edit complete

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -158,7 +158,7 @@ export default function Dashboard() {
           confetti({ particleCount: 35, spread: 50, origin: { y: 0.7 }, ticks: 120 });
           playTaskCompleteSound();
           addXP(t.xpReward);
-          toast.success(`+${t.xpReward} XP! Your Task completed 🎉`);
+          toast.success(`+${t.xpReward} XP! Task completed 🎉`);
         } else {
 
           removeXP(t.xpReward);
